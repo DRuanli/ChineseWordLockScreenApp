242555b9cf25b73236fadbd8702648873a8a8fb5
Elio <nguyen2332004@gmail.com>
2025-09-09 20:32:02 +0700

    first commit

---
+++ WordWidget/WordWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  WordWidgetBundle.swift
+//  WordWidget
+//
+//  Created by Lê Nguyễn on 9/9/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WordWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WordWidget()
+        WordWidgetControl()
+        WordWidgetLiveActivity()
+    }
+}
